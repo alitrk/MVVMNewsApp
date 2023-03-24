e50ae662072f0835bb7b9b9a7137c924db060091
alitrk <turkali750@gmail.com>
2023-03-24 20:52:31 +0300

    Offline caching with NetworkBoundResource

--- app/src/main/java/com/codinginflow/mvvmnewsapp/features/breakingnews/BreakingNewsFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmnewsapp/features/breakingnews/BreakingNewsFragment.kt
@@ -2,6 +2,7 @@ package com.codinginflow.mvvmnewsapp.features.breakingnews
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -9,7 +10,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.codinginflow.mvvmnewsapp.R
 import com.codinginflow.mvvmnewsapp.databinding.FragmentBreakingNewsBinding
 import com.codinginflow.mvvmnewsapp.shared.NewsArticleListAdapter
+import com.codinginflow.mvvmnewsapp.util.Resource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
@@ -30,8 +33,20 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
             }
 
             viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-                viewModel.breakingNews.collect { articles ->
-                    newsArticleAdapter.submitList(articles)
+                viewModel.breakingNews.collect {
+                    val result = it ?: return@collect
+
+                    swipeRefreshLayout.isRefreshing = result is Resource.Loading
+                    recyclerView.isVisible = !result.data.isNullOrEmpty()
+                    textViewError.isVisible = result.error  != null && result.data.isNullOrEmpty()
+                    buttonRetry.isVisible = result.error  != null && result.data.isNullOrEmpty()
+                    textViewError.text = getString(
+                        R.string.could_not_refresh,
+                        result.error?.localizedMessage
+                            ?: getString(R.string.unknown_error_occurred)
+                    )
+
+                    newsArticleAdapter.submitList(result.data)
                 }
             }
         }
